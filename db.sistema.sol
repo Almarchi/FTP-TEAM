dd3170bdfb1585d401951348cc8eb63e651c5a0f
jaltgon <jaltgon773@ieszaidinvergeles.org>
2025-01-10 12:10:31 +0100

    Create db.sistema.sol

---
+++ db.sistema.sol
@@ -0,0 +1,14 @@
+$TTL    604800
+      @       IN      SOA     tierra.sistema.sol. admin.sistema.sol. (
+                            2         ; Serial
+                            604800    ; Refresh
+                            86400     ; Retry
+                            2419200   ; Expire
+                            604800 )  ; Negative Cache TTL
+      ;
+      @       IN      NS      tierra.sistema.sol.
+      @       IN      A       192.168.57.103
+      tierra  IN      A       192.168.57.103
+      mercurio IN     A       192.168.57.101
+      venus   IN      A       192.168.57.102
+      marte   IN      A       192.168.57.104
